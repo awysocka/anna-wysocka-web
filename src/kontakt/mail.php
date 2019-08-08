dff4e55778e1473e5bf3c726a8a8a6ef0ece0705
awysocka <wysocka.anna88@gmail.com>
2019-08-08 23:21:34 +0200

    upgrade npm scripts

--- src/kontakt/mail.php
+++ src/kontakt/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "wysocka.anna88@gmail.com";
+$recipient = "kontakt@annawysocka.pl";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Błąd!");
